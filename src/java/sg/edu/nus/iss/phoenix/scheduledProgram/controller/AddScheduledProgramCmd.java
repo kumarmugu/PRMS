b6cbf822ca0c19328c9e03135c794665e0956a99
Mezaw <Mezaw@WDGCIC-Mezaw.wirecard.lan>
2016-10-02 12:38:45 +0800

    Update in Create Add Schedule Program

--- src/java/sg/edu/nus/iss/phoenix/scheduledProgram/controller/AddScheduledProgramCmd.java
+++ src/java/sg/edu/nus/iss/phoenix/scheduledProgram/controller/AddScheduledProgramCmd.java
@@ -9,22 +9,16 @@ import at.nocturne.api.Action;
 import at.nocturne.api.Perform;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import sg.edu.nus.iss.phoenix.authenticate.entity.User;
 import sg.edu.nus.iss.phoenix.core.exceptions.AnnualSchedueNotExistException;
 import sg.edu.nus.iss.phoenix.scheduledProgram.delegate.ReviewAndSelectScheduledProgramDelegate;
 import sg.edu.nus.iss.phoenix.scheduledProgram.delegate.ScheduledProgramDelegate;
-import sg.edu.nus.iss.phoenix.scheduledProgram.entity.AnnualSchedule;
 import sg.edu.nus.iss.phoenix.scheduledProgram.entity.ProgramSlot;
 import sg.edu.nus.iss.phoenix.scheduledProgram.entity.WeeklySchedule;
-import sg.edu.nus.iss.phoenix.util.DateUtil;
 
 /**
  *
@@ -38,52 +32,7 @@ public class AddScheduledProgramCmd implements Perform {
        
        String year = req.getParameter("year");
        String week = req.getParameter("week");
-       /*       
-       String useraction = req.getParameter("txtcreateAnnualSchedule");
-       
-        User user= (User) req.getSession().getAttribute("user");
-        
-        ProgramSlot  srd = new ProgramSlot();
-      
-         String expectedPattern = "dd/MM/yyyy HH:mm:ss";
-         SimpleDateFormat formatter = new SimpleDateFormat(expectedPattern);       
-         String programStartDateime= req.getParameter("date") + " "+ req.getParameter("startTime")+":00";
-         
-         System.out.println("programStartDateime"+programStartDateime);
-         
-        Date srdStartDate = null;
-        try {
-                srdStartDate = formatter.parse(programStartDateime);            
-                System.out.println("srdStartDate: " + srdStartDate);
-
-        } catch (ParseException ex) {
-            Logger.getLogger(AddScheduledProgramCmd.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        //getTimeDiff(String starttime,String endtime) 
-        
-        String starttime = req.getParameter("startTime");
-        String endtime=req.getParameter("endTime");
-         
-        srd.setProgramName(req.getParameter("program"));   
-        // srd.setProgramName("MM News");
-         System.out.println("==============================================");
-         System.out.println("req.getParameter(\"program\"):  "+req.getParameter("program"));
-        srd.setPresenterId(req.getParameter("presenterId")); 
-        //srd.setPresenterId("mozert"); 
-        System.out.println("srd.getPresenterId():  "+srd.getPresenterId());
-        srd.setProducerId(req.getParameter("producerId"));
-         System.out.println("srd.getProducerId():  "+srd.getProducerId());
-        srd.setupdatedBy(user.getId());
-        srd.setupdatedOn(new Date());        
-        srd.setStartTime(srdStartDate);
-        
-        srd.setweekStartDate(DateUtil.getStartDateOfWeek(year, week));
-        try {
-            srd.setDuration(DateUtil.getTimeDiff(starttime, endtime));
-        } catch (ParseException ex) {
-            Logger.getLogger(AddScheduledProgramCmd.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
+     
                 
         ScheduledProgramDelegate srddel= new ScheduledProgramDelegate();
         ProgramSlot  srd = null;
